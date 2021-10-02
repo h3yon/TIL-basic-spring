fdf297c881809a6e7dc1cd61ae7e44884c0acb1f
h3yon <khykhy1006@gmail.com>
2021-10-03 03:58:10 +0900

    feat: 통합테스트

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -4,6 +4,7 @@ import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,22 +12,16 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-
-    // 스프링이 자체적으로 빈 주입해줌
-    private DataSource dataSource;
-
+    private final DataSource dataSource;
     public SpringConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
-
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
-    public MemberRepository memberRepository(){
-        // 구현체 리턴
+    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
         return new JdbcMemberRepository(dataSource);
     }
