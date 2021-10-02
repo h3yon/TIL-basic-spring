5d6b2243b8be46555d3719351373d7c893b95763
h3yon <khykhy1006@gmail.com>
2021-10-03 02:44:55 +0900

    feat: 자바 코드로 직접 스프링 빈 등록

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     // 동시성 문제로 cuncurrent~ 추가해야될 수 있음
